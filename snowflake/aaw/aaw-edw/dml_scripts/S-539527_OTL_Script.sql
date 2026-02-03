55ee3882c00d9c2bef338b25ef9917e8cfc1f659
Harshitha KG <harshithaalt@gmail.com>
2026-02-03 13:38:27 +0530

    Update S-539527_OTL_Script.sql

--- snowflake/aaw/aaw-edw/dml_scripts/S-539527_OTL_Script.sql
+++ snowflake/aaw/aaw-edw/dml_scripts/S-539527_OTL_Script.sql
@@ -1,7 +1,7 @@
 USE ROLE SYSADMIN;
 USE WAREHOUSE COMPUTE_WH;
-INSERT INTO SNOWFLAKE_LEARNING_DB.PUBLIC.customers (first_name, last_name, email)
-VALUES 
-    ('John', 'Doe', 'john.doe@email.com'),
-    ('Jane', 'Smith', 'jane.smith@email.com'),
-    ('Bob', 'Johnson', 'bob.johnson@email.com');
+CREATE TABLE SNOWFLAKE_LEARNING_DB.PUBLIC.customers (
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
