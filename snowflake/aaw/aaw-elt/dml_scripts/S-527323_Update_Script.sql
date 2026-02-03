53f8dff0c0d9eafb8afff778e035296807e6f591
Harshitha KG <harshithaalt@gmail.com>
2026-02-03 10:50:03 +0530

    Update S-527323_Update_Script.sql

--- snowflake/aaw/aaw-elt/dml_scripts/S-527323_Update_Script.sql
+++ snowflake/aaw/aaw-elt/dml_scripts/S-527323_Update_Script.sql
@@ -5,5 +5,5 @@ USE WAREHOUSE COMPUTE_WH;
 CREATE TABLE customers (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
