905a5e00d15de80a1fa67eef4e47a333702d8639
Harshitha KG <harshithaalt@gmail.com>
2026-02-03 15:22:53 +0530

    Update S-527323_Update_Script.sql

--- snowflake/aaw/aaw-elt/dml_scripts/S-527323_Update_Script.sql
+++ snowflake/aaw/aaw-elt/dml_scripts/S-527323_Update_Script.sql
@@ -2,7 +2,7 @@
 USE ROLE SYSADMIN;
 USE WAREHOUSE COMPUTE_WH;
 
-INSERT INTO customers (first_name, last_name, email)
+INSERT INTO SNOWFLAKE_LEARNING_DB.PUBLIC.CUSTOMERS (first_name, last_name, email)
 VALUES 
     ('John', 'Doe', 'john.doe@email.com'),
     ('Jane', 'Smith', 'jane.smith@email.com'),
